cefd7d45df45820f7afbd0f6d6448ab2ccc1e248
Ben Ristow <144918585+JustAdd-GAMES@users.noreply.github.com>
2025-07-02 11:28:36 +0200

    animcontrol

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -8,13 +8,15 @@ public class MovementScript : MonoBehaviour
     [SerializeField] public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    Animator anim;
+
 
 
     void Start()
     {
         //assign rigidbody
         rb = this.GetComponent<Rigidbody2D>();
-
+        anim = GetComponent<Animator>();
     }
 
 
@@ -22,14 +24,23 @@ public class MovementScript : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        //animation
+
     }
 
     void FixedUpdate()
     {
         // Normalize the input to prevent faster diagonal movement
-         movement.Normalize();
-        rb.linearVelocity = movement * moveSpeed; 
+        movement.Normalize();
+        rb.linearVelocity = movement * moveSpeed;
     }
 
 
+    void Aninmate()
+    {
+        anim.SetFloat("moveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("moveY", Input.GetAxisRaw("Vertical"));
+        anim.SetFloat("MoveMagnitude", movement.magnitude);
+    }    
+    
 }
